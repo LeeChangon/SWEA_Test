444f691ad7ed9431d0b7bb28c8d654343ae3e188
Ongon <49462670+LeeChangon@users.noreply.github.com>
2022-08-25 21:45:49 +0900

    Update fly_elimination_SWEA_2001.py

--- D2/파리 퇴치/fly_elimination_SWEA_2001.py
+++ D2/파리 퇴치/fly_elimination_SWEA_2001.py
@@ -1,5 +1,20 @@
 T = int(input())
-
+ 
 for tc in range(1, T + 1):
-    N, M = map(int, input())
-    print(N, M)
+    N, M = map(int, input().split())
+    fly = []
+    mx = 0
+    for i in range(N):
+        fly.append(list(map(int, input().split())))
+    for i in range(N - (M - 1)):
+        for j in range(N - (M - 1)):
+            dead = 0
+            for x in range(M):
+                for y in range(M):
+                    dead += fly[i + x][j + y]   #범위 만큼 파리를 잡는다
+             
+            if(mx < dead):
+                mx = dead               #최대로 잡힌 파리 수
+             
+    print("#", tc, sep = "", end = " ")
+    print(mx)
